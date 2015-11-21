f9f750aa0491c98d8c7e57c8c8489eff8f3548b8
blasian <mikespearman.e@gmail.com>
2015-11-21 13:12:00 -0800

    Initial Commit

---
+++ DealSecret/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DealSecret
+//
+//  Created by Michael Spearman on 11/21/15.
+//  Copyright © 2015 Michael Spearman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DealSecret/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DealSecret
+//
+//  Created by Michael Spearman on 11/21/15.
+//  Copyright © 2015 Michael Spearman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
